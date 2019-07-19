dadd8d2e9dca8166876707930a1986fb193f1ad3
Rapid-SBS <info@rapid-sbs.com>
2019-07-18 22:01:43 -0700

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-var express = require('express');
-var app = express();
-var port = 8000;
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongo = require('mongodb').MongoClient;
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const app = express();
+const port = 8000;
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Routing
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+/* 
+app.get('/', (req, res, next) => {
+	res.send('Hello! The time and date is: ' + new Date());
+});
+
+app.get('/json', (req, res, next) => {
+	const data = {
+		key: 'Value'
+	}
+
+	res.json(data);
+})
+*/
+// -----------------
 
 app.listen(port, function(err, res) {
 	if (err) {
