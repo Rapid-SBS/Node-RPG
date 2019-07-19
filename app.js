27351f0b1b644797f5413d4f7858e42c7297f370
Rapid-SBS <info@rapid-sbs.com>
2019-07-19 15:48:02 -0700

    Handlebar Helpers
    Added Handlebar Helpers page with examples of the main Helpers available in Handlebars

--- app.js
+++ app.js
@@ -8,13 +8,55 @@ const url = require('url');
 const app = express();
 const port = 8000;
 
-app.set('views', path.join(__dirname, 'views'))
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ 
+	defaultLayout: 'main',
+	layoutsDir: path.join(__dirname, 'views/pagelayouts')
+}));
 
-// Routing
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'))
+
+// ----- Routing -----
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', { 
+		title: 'Home',
+		header: 'RPGenius',
+		description: 'An online tool for curating a D&D-Lite experience.'
+	});
+});
+
+app.get('/test', (req, res) => {
+	res.render('test', { 
+		title: 'Test Page',
+		description: 'This is a test-page description'
+	});
+});
+
+app.get('/helpers', (req, res) => {
+	res.render('helpers', { 
+		title: 'Helpers',
+		description: 'A list of Handlebar Helpers for conditional logic.',
+		ifval: true,
+		unlessval: false,
+		eacharray: [
+			"Array Item One",
+			"Array Item Two",
+			"Array Item Three"
+		],
+		eachobject: {
+			keyone: "Value 1",
+			keytwo: "Value 2",
+			keythree: "Value 3"
+		},
+		arrayofobjects: [
+			{
+				fruits: ['Mango', 'Peach', 'Banana']
+			},
+			{
+				fruits: ['Apple', 'Orange', 'Strawberry']
+			}
+		]	
+	});
 });
 
 /* 
